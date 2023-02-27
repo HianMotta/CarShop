f088ba1ba9d84d81b70d7d53a699842ca2127cc0
Hian <hian.hsm@gmail.com>
2023-02-27 11:59:12 -0300

    feat: motorcycle controller

---
+++ src/Controllers/MotorcycleController.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import MotorcycleService from '../Services/MotorcycleService';
+
+class MotorcycleController {
+  private req: Request;
+  private res: Response;
+  private next: NextFunction;
+  private service: MotorcycleService;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+    this.service = new MotorcycleService();
+  }
+
+  public async create() {
+    try {
+      const motorcycle: IMotorcycle = this.req.body;
+      const newMotorcycle = await this.service.create(motorcycle);
+      return this.res.status(201).json(newMotorcycle);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+}
+
+export default MotorcycleController;
