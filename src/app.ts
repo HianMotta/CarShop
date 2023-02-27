0075be36d5e20fd74e1ff9bdfde664720756c636
Hian <hian.hsm@gmail.com>
2023-02-27 12:02:43 -0300

    feat: create motorcycle route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import CarRoutes from './Routes/CarRoutes';
+import MotorcycleRoutes from './Routes/MotorcycleRoutes';
 import httpErrorMiddleware from './middlewares/httpErrorMiddleware';
 
 const app = express();
 
 app.use(express.json());
 app.use('/cars', CarRoutes);
+app.use('/motorcycles', MotorcycleRoutes);
 app.use(httpErrorMiddleware);
 
 export default app;
